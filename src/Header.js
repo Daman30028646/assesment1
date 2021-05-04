28b9773d4d4e24c3da9ce02074ced3d295962d8d
Damanpreet  Singh <30028646@student.toiohomai.ac.nz>
2021-05-04 13:45:38 +1200

    still updating

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ class Header extends Component
         return(
 <div>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Welcome</a>
+  <a class="navbar-brand">Welcome</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
